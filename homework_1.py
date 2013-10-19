356ffb44e5a9ebf7f9e2f5cb50d334bdaeb96099
TN1ck <tomwanick@gmail.com>
2013-10-19 20:07:21 +0200

    corrected generate_data

--- homework_1.py
+++ homework_1.py
@@ -9,64 +9,64 @@ import pdb
 ''' ---- Task 1 ---- '''
 
 def task1():
-    ''' 
+    '''
     Task 1
     Generate, transform and plot gaussian data
     '''
     X = generate_data(100)
     X2 = scale_data(X)
-    X3 = standardise_data(X) 
-    
-    # Plot data 
-    # Your code here 
-    # Hint: Use the functions pl.scatter(x[0,:],[1,:],c='r'), pl.hold(True), 
-    # pl.legend, pl.title, pl.xlabel, pl.ylabel     
-    
+    X3 = standardise_data(X)
+
+    # Plot data
+    # Your code here
+    # Hint: Use the functions pl.scatter(x[0,:],[1,:],c='r'), pl.hold(True),
+    # pl.legend, pl.title, pl.xlabel, pl.ylabel
+
 def generate_data(N):
     '''
-    Generate N data points form a 2D Gaussian Gaussian distribution 
-    with mean [1, 2] 
-    
+    Generate N data points form a 2D Gaussian Gaussian distribution
+    with mean [1, 2]
+
     Usage:     x = generate_data(N)
-               
-    Returns:   x : a 2xN array    
-    
+
+    Returns:   x : a 2xN array    [[1...N], [1...N]]
+
     Instructions: Use sp.random.mutivariate_normal
     '''
     mean = [1, 2]
-    cov = [[1, 0], [0, 1]]
-    return sp.random.multivariate_normal(mean, cov, (N))
+    cov = sp.identity(2)
+    return sp.random.multivariate_normal(mean, cov, (N)).T
 
 def scale_data(X):
     '''
     Scales the data in X by 2 in x-direction and by 0.5 in y-direction
-    
+
     Usage:     Y = scale_data(X)
-    Input:     X : a 2xN array 
+    Input:     X : a 2xN array
     Returns:   Y : a 2xN array of scaled data
-    
+
     '''
     x, y = X
     return [map(lambda c: c * 2, x), map(lambda c: c * 0.5, y)]
-    
+
 def standardise_data(X):
     ''' Returns a centered, scaled version of X, the same size as X.
-    
+
     Usage:      Y = standardise_data(X)
-    Input:      X : a DxN array 
+    Input:      X : a DxN array
     Returns:    Y : a DxN array of z-scores of X
                     Y[i][n] = (X[i][n] - mean(X[i][:]))/std(X[i][:])
-    
+
     Instructions: Do not use for-loops. Use sp.mean and sp.std
     '''
     print map(lambda c: map(lambda i: i - sp.mean(c)/sp.std(c), c), X)
-    
+
 ''' ---- Task 2 ---- '''
 
 def task2():
-    ''' 
-    Task 2 
-    Calculate time demand of different mean calculations 
+    '''
+    Task 2
+    Calculate time demand of different mean calculations
     (for-loop based implementation vs. scipy.mean)
     '''
     dims = [100, 1000, 10**4, 10**5, 10**6]
@@ -76,36 +76,36 @@ def task2():
         r2 = timedcall(sp.mean, x,1)
         print 'For N = ' + str(d) + ' scipy.mean is ' + str(r1 - r2) \
             + 's faster than a for-loop implementation'
-    
+
 def mean_for(X):
     ''' Mean of array X along the rows
-    
+
     Usage:      m = mean_for(X)
-    Input:      X : a DxN array 
+    Input:      X : a DxN array
     Returns:    m : a Dx1 array containing the means of each row
-    
-    Example: if   X =  [1 5      mean_for(X) = [3 
-                        2 6                     4 
+
+    Example: if   X =  [1 5      mean_for(X) = [3
+                        2 6                     4
                         3 7]                    5]
-         
-        
-    Instructions: Use for-loops to replicate sp.mean(X,1) 
+
+
+    Instructions: Use for-loops to replicate sp.mean(X,1)
     Do not use sp.mean or sp.sum
     '''
-    # Your code here 
+    # Your code here
 
 
 def timedcall(fn, *args):
     '''Call function with args; return the time in seconds and result.
-        example: 
-        You want to time the function call "C = foo(A,B)". 
+        example:
+        You want to time the function call "C = foo(A,B)".
         --> "T, C = timecall(foo, A, B)"
     '''
     t0 = time.clock()
     result = fn(*args)
     t1 = time.clock()
     return t1-t0
-    
+
 ''' ---- Function for testing ---- '''
 
 def test_prep():
@@ -119,7 +119,7 @@ def test_prep():
     c = sp.concatenate((a,b),axis=0)
     assert(sp.all(sp.mean(standardise_data(c),1).round() == sp.zeros((1,4))))
     #test mean_for
-    assert(sp.all(mean_for(a) == sp.mean(a,1))) 
+    assert(sp.all(mean_for(a) == sp.mean(a,1)))
     #test generate_data
     x = generate_data(200)
     assert(x.shape == (2, 200))
