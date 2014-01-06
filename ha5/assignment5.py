8edcdf415064924baa3aac23153d235f1c19c5a9
TN1ck <tomwanick@gmail.com>
2014-01-06 20:43:23 +0100

    completed first exercise

--- ha5/assignment5.py
+++ ha5/assignment5.py
@@ -20,10 +20,10 @@ def load_data(fname):
     Y_train = Y_train[:, :1000]
     return X_train,Y_train
 
-def GaussianKernel(X1, X2, kwdith):
-    ''' Compute Gaussian Kernel 
-    Input: X1    - DxN1 array of N1 data points with D features 
-           X2    - DxN2 array of N2 data points with D features 
+def GaussianKernel(X1, X2, kwidth):
+    ''' Compute Gaussian Kernel
+    Input: X1    - DxN1 array of N1 data points with D features
+           X2    - DxN2 array of N2 data points with D features
            kwidth - Kernel width
     Output K     - N1 x N2 Kernel matrix
     '''
@@ -31,64 +31,74 @@ def GaussianKernel(X1, X2, kwdith):
     K = cdist(X1.T, X2.T, 'euclidean')
     K = np.exp(-(K ** 2) / (2. * kwidth ** 2))
     return K
-	
+
 def train_krr(X_train, Y_train,kwidth,llambda):
     ''' Trains kernel ridge regression (krr)
     Input:       X_train  -  DxN array of N data points with D features
                  Y        -  D2xN array of length N with D2 multiple labels
                  kwdith   -  kernel width
                  llambda    -  regularization parameter
-    Output:      alphas   -  NxD2 array, weighting of training data used for apply_krr                     
+    Output:      alphas   -  NxD2 array, weighting of training data used for apply_krr
     '''
     # your code here
-    
+    K = GaussianKernel(X_train, X_train, kwidth)
+    foo = K + (llambda * np.identity(K.shape[0]))
+    baz = np.linalg.inv(foo)
+    alpha = baz.dot(Y_train.T)
+    return alpha
+
 def apply_krr(alphas, X_train, X_test, kwidth):
     ''' Applys kernel ridge regression (krr)
-    Input:      alphas      -  NtrxD2 array trained in train_krr      
+    Input:      alphas      -  NtrxD2 array trained in train_krr
                 X_train     -  DxNtr array of Ntr train data points with D features
                 X_test      -  DxNte array of Nte test data points with D features
-                kwidht      -  Kernel width             
+                kwidht      -  Kernel width
     Output:     Y_test      -  D2xNte array
     '''
     # your code here
-	
+    k = GaussianKernel(X_test, X_train, kwidth)
+    Y_test = k.dot(alphas).T
+    return Y_test
+
+
+
 def train_ols(X_train, Y_train):
-    ''' Trains ordinary least squares (ols) regression 
+    ''' Trains ordinary least squares (ols) regression
     Input:       X_train  -  DxN array of N data points with D features
                  Y        -  D2xN array of length N with D2 multiple labels
-    Output:      W        -  DxD2 array, linear mapping used to estimate labels 
-                             with sp.dot(W.T, X)                      
+    Output:      W        -  DxD2 array, linear mapping used to estimate labels
+                             with sp.dot(W.T, X)
     '''
     #W = sp.dot(inv(sp.dot(X_train,X_train.T)),sp.dot(X_train,Y_train.T))
     W = solve(sp.dot(X_train,X_train.T),sp.dot(X_train,Y_train.T))
     return W
-    
+
 def apply_ols(W, X_test):
-    ''' Applys ordinary least squares (ols) regression 
+    ''' Applys ordinary least squares (ols) regression
     Input:       X_test    -  DxN array of N data points with D features
-                 W        -  DxD2 array, linear mapping used to estimate labels 
-                             trained with train_ols                   
+                 W        -  DxD2 array, linear mapping used to estimate labels
+                             trained with train_ols
     Output:     Y_test    -  D2xN array
     '''
     Y_test = sp.dot(W.T, X_test)
     return Y_test
-	
+
 def test_handpositions():
     X,Y = load_data('myo_data.mat')
     crossvalidate_krr(X,Y)
-		
+
 def test_sine_toydata(kwidth = 1, llambda = 1):
 	#Data generation
     X_train = sp.arange(0,10,.01)
     X_train = X_train[None,:]
     Y_train = sp.sin(X_train) + sp.random.normal(0,.5,X_train.shape)
-    #Linear Regression 
-    w_est = train_ols(X_train, Y_train) 
+    #Linear Regression
+    w_est = train_ols(X_train, Y_train)
     Y_est_lin = apply_ols(w_est,X_train)
     #Kernel Ridge Regression
     alphas = train_krr(X_train,Y_train,kwidth,llambda)
     Y_est_krr = apply_krr(alphas,X_train,X_train,kwidth)
-    
+
     #Plot result
     pl.figure()
     pl.plot(X_train.T, Y_train.T, '+k', label = 'Train Data')
@@ -97,15 +107,16 @@ def test_sine_toydata(kwidth = 1, llambda = 1):
     pl.xlabel('x')
     pl.ylabel('y')
     pl.legend(loc = 'lower right')
-    
-      
+
+test_sine_toydata(1,1)
+
 def crossvalidate_krr(X,Y,f=5, kwidths=10.0**np.array([0, 1, 2]), llambdas=10.0**np.array([-4, -2, 0])):
-    ''' 
+    '''
     Test generalization performance of kernel ridge regression with gaussian kernel
     Input:	    X	data (dims-by-samples)
     			Y	labels (dims2-by-samples)
     			f	number of cross-validation folds
-    			kwidths width of gaussian kernel function 
+    			kwidths width of gaussian kernel function
     			llambdas regularizer (height of ridge on kernel matrix)
     '''
     N = f*(X.shape[-1]/f)
@@ -113,7 +124,7 @@ def crossvalidate_krr(X,Y,f=5, kwidths=10.0**np.array([0, 1, 2]), llambdas=10.0*
     r2_outer = sp.zeros((f))
     r2_linear = sp.zeros((f))
     r2_inner = sp.zeros((f-1,kwidths.shape[-1],llambdas.shape[-1]))
-    
+
     # to outer cross-validation (model evaluation)
     for ofold in range(f):
         # split in training and test (outer fold)
@@ -121,7 +132,7 @@ def crossvalidate_krr(X,Y,f=5, kwidths=10.0**np.array([0, 1, 2]), llambdas=10.0*
         otestidx[ofold] = 1
         otest = idx[otestidx,:].flatten()
         otrain = idx[~otestidx,:]
-        
+
         # inner cross-validation (model selection)
         for ifold in range(f-1):
             # split in training and test (inner fold)
@@ -129,7 +140,7 @@ def crossvalidate_krr(X,Y,f=5, kwidths=10.0**np.array([0, 1, 2]), llambdas=10.0*
             itestidx[ifold] = 1
             itest = otrain[itestidx,:].flatten()
             itrain = otrain[~itestidx,:].flatten()
-            
+
             # do inner cross-validation (model selection)
             for illambda in range(llambdas.shape[-1]):
                 for ikwidth in range(kwidths.shape[-1]):
@@ -145,12 +156,12 @@ def crossvalidate_krr(X,Y,f=5, kwidths=10.0**np.array([0, 1, 2]), llambdas=10.0*
         alphas = train_krr(X[:,otrain.flatten()],Y[:,otrain.flatten()], kwidths[optkwidthidx],llambdas[optllambdaidx])
         yhat = apply_krr(alphas, X[:,otrain.flatten()],X[:,otest], kwidths[optkwidthidx])
         r2_outer[ofold] = compute_rsquare(yhat,Y[:,otest])
-        				
+
         # for comparison: predict with linear model
-        w_est = train_ols(X[:,otrain.flatten()], Y[:,otrain.flatten()]) 
+        w_est = train_ols(X[:,otrain.flatten()], Y[:,otrain.flatten()])
         y_est_lin = apply_ols(w_est,X[:,otest])
         r2_linear[ofold] = compute_rsquare(y_est_lin,Y[:,otest])
-        
+
         print 'Fold %d'%ofold + ' best kernel width %f'%kwidths[optkwidthidx] +\
         ' best regularizer %f'%llambdas[optllambdaidx] + \
         ' rsquare %f'%r2_outer[ofold] + \
@@ -161,7 +172,7 @@ def crossvalidate_krr(X,Y,f=5, kwidths=10.0**np.array([0, 1, 2]), llambdas=10.0*
     pl.xticks((1,2),('KRR','Lin'))
     pl.savefig('krr_vs_lin_comparison.pdf')
     return r2_outer,r2_linear
-    
+
 def compute_rsquare(yhat,Y):
     '''compute coefficient of determination'''
     return 1 - (sp.var((yhat - Y),axis=1).sum()/sp.var(Y,axis=1).sum())
